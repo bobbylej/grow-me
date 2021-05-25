d90f97fc6ee9b147e884bbd0896641322656324d
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2021-05-25 12:29:09 +0100

    [29] Add styles

---
+++ src/app/shared/components/FormControl/FormControl.styles.ts
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/styles';
+
+export const useFormControlStyles = makeStyles(() => ({
+  formControlWrapper: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+}));
