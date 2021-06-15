f75d50a6022938b0bb84718e47872e93045ed2e8
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2021-06-15 11:32:15 +0100

    [35] Add labelAnswer component

---
+++ src/app/shared/components/LabelAnswer/LabelAnswer.styles.ts
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/styles';
+
+export const useLabelAnswerStyles = makeStyles(() => ({
+  labelContainer: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+  },
+  labelWrapper: {
+    width: '30%',
+  },
+}));
