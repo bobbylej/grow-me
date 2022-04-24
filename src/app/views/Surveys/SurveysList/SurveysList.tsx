a6df504de6a1f5d51dfa49eb008ce11be29c5ab1
Mateusz <mateusz.adamczyk@monterail.com>
2022-04-24 09:21:18 +0200

    [temp] Test cookies

--- src/app/views/Surveys/SurveysList/SurveysList.tsx
+++ src/app/views/Surveys/SurveysList/SurveysList.tsx
@@ -1,17 +1,17 @@
-import { useIntl } from 'react-intl';
-import React, { ReactElement, useEffect, useState } from 'react';
 import { Fab, Grid } from '@material-ui/core';
 import { Link, useHistory, useRouteMatch } from 'react-router-dom';
+import React, { ReactElement, useEffect, useState } from 'react';
+
 import AddIcon from '@material-ui/icons/Add';
+import { useIntl } from 'react-intl';
+import { FabContainer } from 'app/shared/components/FabContainer/FabContainer';
 import { ItemSquare } from 'app/shared/components/ItemSquare/ItemSquare';
 import { SquareItem } from 'app/shared/interfaces/squareItem.interface';
+import { Survey } from 'app/shared/interfaces/survey.interface';
+import { getSurveys } from 'app/shared/api/services/surveys.service';
+import { useFabStyles } from 'app/shared/styles/fab.styles';
 import useLayoutStyles from 'app/shared/styles/layout.styles';
 import { usePageTitle } from 'app/shared/hooks/usePageTitle';
-import { useFabStyles } from 'app/shared/styles/fab.styles';
-import { FabContainer } from 'app/shared/components/FabContainer/FabContainer';
-import { getSurveys } from 'app/shared/api/services/surveys.service';
-
-import { Survey } from 'app/shared/interfaces/survey.interface';
 
 export const SurveysList = (): ReactElement => {
   const intl = useIntl();
@@ -60,12 +60,25 @@ export const SurveysList = (): ReactElement => {
     </Grid>
   ));
 
+  const test = () => {
+    document.cookie = `${'test_lax_1'}=${'Testssss'}; Domain=${'netlify.app'}; Path=/; SameSite=Lax; Secure`;
+    document.cookie = `${'test_lax_2'}=${'Testssss'}; Domain=${'.netlify.app'}; Path=/; SameSite=Lax`;
+    document.cookie = `${'test_no_samesite_1'}=${'Testssss'}; Domain=${'netlify.app'}; Path=${'/'}`;
+    document.cookie = `${'test_no_samesite_2'}=${'Testssss'}; Domain=${'.netlify.app'}; Path=${'/'}`;
+    document.cookie = `${'test_no_domain_1'}=${'Testssss'}; Path=${'/'}`;
+    document.cookie = `${'test_no_domain_2'}=${'Testssss'}; Path=${'/'}; SameSite=Lax`;
+    // window.location.href = 'https://growme.netlify.app';
+  };
+
   return (
     <Grid
       className={`${content} ${listContainer}`}
       container
       spacing={3}
     >
+      <button type="button" onClick={test}>
+        Test
+      </button>
       {itemsSquareList}
       <Grid item xs={12}>
         <FabContainer>
